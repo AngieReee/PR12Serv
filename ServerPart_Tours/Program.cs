80786542236e75aea1d73597bbbb62fa1e52e5dc
AngelikaSuhareva <asuhareva356@gmail.com>
2025-01-21 10:59:53 +0300

    add tours repository and service

--- ServerPart_Tours/Program.cs
+++ ServerPart_Tours/Program.cs
@@ -1,9 +1,14 @@
 using ServerPart_Tours.Models;
+using ServerPart_Tours.Repositories;
+using ServerPart_Tours.Servises;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<_43pToursContext>();
-
+builder.Services.AddScoped<ToursRepository>();
+builder.Services.AddScoped<ToursService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
---
+++ ServerPart_Tours/Servises/ToursService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using ServerPart_Tours.Repositories;
+
+namespace ServerPart_Tours.Servises
+{
+    public class ToursService
+    {
+        readonly ToursRepository repository;
+        
+        public ToursService(ToursRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        /// <summary>
+        /// преобразование списка со всеми турами в json
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IResult> GetToursList() => Results.
+            Text(JsonConvert.SerializeObject(
+                await repository.GetTours(),
+                Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                }));
+    }
+}
